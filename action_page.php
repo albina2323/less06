3bccef5e56479f226e6ed4de0308237e54cdbfe1
albina <154615995+albina2323@users.noreply.github.com>
2023-12-27 08:38:49 +0300

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,46 @@
+<?php  
+  echo "<pre>"; 
+  print_r($_SERVER); 
+  print_r($_POST); 
+  echo "</pre>"; 
+ 
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "db"; 
+ 
+// Create connection 
+$db = new mysqli($servername, $username, $password, $dbname); 
+// Check connection 
+if ($db->connect_error) { 
+  die("Connection failed: " . $db->connect_error); 
+} 
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    // collect value of input field 
+    $firstname = $_POST['fname']; 
+    $lastname = $_POST['lname']; 
+    $country = $_POST['country']; 
+    $subject = $_POST['subject']; 
+     
+   // if (empty($name)) { 
+   //   echo "Name is empty"; 
+    //} else { 
+   //   echo $name; 
+   // } 
+   $sql = "INSERT INTO myuser (firstname, lastname, country, subject) 
+VALUES ('$firstname', '$lastname', '$country' ,'$subject')"; 
+if ($db->query($sql) == TRUE) { 
+    echo "New record created successfully <a href='http://less06/'>Главная стра....</a>"; 
+  } else { 
+    echo "Error: " . $sql . "<br>" . $db->error; 
+  } 
+  } 
+ 
+ 
+ 
+ 
+ 
+$db->close(); 
+ 
+ 
+?>
